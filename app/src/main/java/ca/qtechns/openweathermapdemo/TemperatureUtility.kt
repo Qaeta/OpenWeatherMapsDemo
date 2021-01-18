d567897598f8582fe24bb18270cbae9389d68380
Erik Heinze-Milne <erik@qtechns.ca>
2021-01-17 21:03:09 -0400

    added documentation to TemperatureUtility

--- app/src/main/java/ca/qtechns/openweathermapdemo/TemperatureUtility.kt
+++ app/src/main/java/ca/qtechns/openweathermapdemo/TemperatureUtility.kt
@@ -1,14 +1,30 @@
+/**
+ * @author Erik Heinze-Milne
+ */
 package ca.qtechns.openweathermapdemo
 
-import java.math.BigDecimal
-import java.math.RoundingMode
 import kotlin.math.roundToInt
 
+/**
+ * Utility for temperature conversions
+ */
 object TemperatureUtility {
+    /**
+     * Converts Kelvin temperature to Celsius
+     *
+     * @param tempInKelvin The temperature in Kelvin
+     * @return Rounded int in Celsius
+     */
     fun ConvertToCelsius(tempInKelvin: Double): Int {
         return (tempInKelvin-273.15).roundToInt()
     }
 
+    /**
+     * Converts Kelvin temperature to Farenheit
+     *
+     * @param tempInKelvin The temperature in Kelvin
+     * @return Rounded int in Farenheit
+     */
     fun ConvertToFarenheit(tempInKelvin: Double): Int {
         return (tempInKelvin*9/5-459.67).roundToInt()
     }
